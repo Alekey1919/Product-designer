1ce208bc92104e8a39f5f8aca1bc3f3017e334dc
unknown <alejosa1919@gmail.com>
2021-06-10 00:44:47 -0300

    Deployment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./Components/Navbar/Custom.scss";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
